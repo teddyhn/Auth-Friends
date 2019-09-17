70dfdc29c6a7aea6c6c30db04aaafdad3b5fefed
teddyhn <tngo97@gmail.com>
2019-09-17 12:48:47 -0700

    Built out Login component

---
+++ friends/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const Login = () => {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = evt => {
+        setCredentials({ ...credentials, [evt.target.name]: evt.target.value })
+    };
+
+    const login = evt => {
+        evt.preventDefault();
+        axiosWithAuth()
+          .post('/login', credentials)
+          .then(res => {
+            localStorage.setItem('token', res.data.payload);
+          })
+          .catch(err => console.log(err));
+    };
+
+    return (
+        <div>
+            <h1>Log In!!</h1>
+            <form onSubmit={login}>
+                Username:
+                <input
+                    type="text"
+                    name="username"
+                    value={credentials.username}
+                    onChange={evt => handleChange(evt)} />
+                Password:
+                <input
+                    type="password"
+                    name="password"
+                    value={credentials.password}
+                    onChange={evt => handleChange(evt)} />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
