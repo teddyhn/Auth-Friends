a7e2426d270d17714d0f6791074ebb8f70f7389a
teddyhn <tngo97@gmail.com>
2019-09-17 12:52:15 -0700

    Login redirects to /friends

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = props => {
     const [credentials, setCredentials] = useState({
         username: '',
         password: ''
@@ -17,6 +17,7 @@ const Login = () => {
           .post('/login', credentials)
           .then(res => {
             localStorage.setItem('token', res.data.payload);
+            props.history.push('/friends');
           })
           .catch(err => console.log(err));
     };
